701cb5b70c3b012926a3c293538569d7fc627e66
Maderna <mda_tmaderna@lnexternos.com.ar>
2026-03-21 20:52:55 -0300

    Se agrego el panel de admnistración en /admin para realizar CRUDS en el grupo PELICULAS, además se agregaron las formas de filtro

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Pelicula, Usuario, Sala
 # Register your models here.
+
+#admin.site.register(Pelicula)
+#admin.site.register(Usuario)
+#admin.site.register(Sala)
+
+@admin.register(Pelicula)
+class PeliculaAdmin(admin.ModelAdmin):
+    list_display=["nombre","autor","fecha_estreno","genero"]
+    list_filter = ["nombre","autor","genero"]
+    ordering = ["-fecha_estreno"]
